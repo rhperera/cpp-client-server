afb1fe02e09025c2e7a47c4d9901c03790b94d42
rhperera <harith9127@gmail.com>
2017-04-27 08:27:30 -0400

    add socket libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include<stdio.h>
 #include "../include/chatmessage.h"
 
+/*
+socket libraries
+*/
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+//hard coded port and buffer
+#define PORT "5000"
+#define DEFAULT_BUFLEN 512
+
 int main(int argc, char const *argv[])
 {
 	printf("Hello\n");
